fffc80424f6e2e2ab753925e34bd11e1aca11d28
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-09-12 09:11:16 -0300

    adiciona campo role na tabela users. Adiciona a coluna 'role' após o campo 'email' na tabela 'users'.

--- database/migrations/2026_09_12_120237_add_role_to_users_table--table=users.php
+++ database/migrations/2026_09_12_120237_add_role_to_users_table--table=users.php
@@ -6,19 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+   
     public function up(): void
     {
-        //
+         Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('usuario')->after('email');
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+   
     public function down(): void
     {
-        //
+         Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
     }
 };
